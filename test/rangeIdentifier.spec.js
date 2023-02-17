071c4ce8178a95a2102e6ac4457848c7744e97ee
Sakthivel Karthikeyan <32382572+myselfsakthivel@users.noreply.github.com>
2023-02-17 15:47:08 +0530

    Create rangeIdentifier.spec.js

---
+++ test/rangeIdentifier.spec.js
@@ -0,0 +1,23 @@
+const {expect} = require('chai');
+const getRangeOfCurrents = require('rangeIdentifier');
+
+describe('currentRange', () => {
+    it('should return message "No range of Values "for empty array', () => {
+        let res = getRangeOfCurrents([]);
+        expect(res).to.equal('No range of values detected');
+    });
+    it('should return range "1-1:1 for single input value', () => {
+        let res = getRangeOfCurrents([1]);
+        expect(res).to.include("{1-1:1}");
+    })
+    it('should return range "4-5:2" for [4,5] input range', () => {
+        let res = getRangeOfCurrents([4, 5]);
+        expect(res).to.include("{4-5:2}");
+    });
+    it('should return range of values for multiple current ranges', () => {
+        const mockData = [1, 2, 3, 3, 4, 7, 8, 9];
+        let res = getRangeOfCurrents(mockData)
+        expect(res).to.include('{1-4:5}');
+        expect(res).to.include('{7-9:3}');
+    });
+});
